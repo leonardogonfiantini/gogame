0bd268616815e4d99396659b4aaa2b6f5090de82
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-09-21 13:11:00 +0200

    a stupid game with dr.regius and mushrooms

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"log"
+	"math/rand"
+	"time"
+	"container/list"
+	"fmt"
+
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
@@ -9,15 +14,21 @@ import (
 
 // Our game constants
 const (
-	screenWidth, screenHeight = 640, 640
+	screenWidth, screenHeight = 800, 600
+	characterCentering = 32
+	mushroomSize = 5
+	catchSize = 32
 )
 
 // Create our empty vars
 var (
 	err        error
 	background *ebiten.Image
-	spaceShip  *ebiten.Image
+	character  *ebiten.Image
+	edulis *ebiten.Image
 	playerOne  player
+	mushrooms  *list.List
+	points int
 )
 
 // Create the player class
@@ -27,34 +38,92 @@ type player struct {
 	speed      float64
 }
 
+type mushroom struct {
+	image *ebiten.Image
+	xPos, yPos float64
+}
+
 // Run this code once at startup
 func init() {
-	background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
+	rand.Seed(time.Now().UnixNano())
+
+	background, _, err = ebitenutil.NewImageFromFile("assets/caucaso.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	spaceShip, _, err = ebitenutil.NewImageFromFile("assets/rocket.png", ebiten.FilterDefault)
+	character, _, err = ebitenutil.NewImageFromFile("assets/me.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	playerOne = player{spaceShip, (screenWidth / 2.0) - 32, (screenHeight / 2.0) - 32, 10}
+	edulis, _, err = ebitenutil.NewImageFromFile("assets/mushroom.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	playerOne = player{character, (screenWidth / 2.0) - characterCentering, (screenHeight / 2.0) - characterCentering, 10}
+	mushrooms = list.New()
+
+}
+
+func createMushroom() mushroom {
+	return mushroom{edulis, float64(rand.Intn(screenWidth-50)), float64(rand.Intn(screenHeight-50))}
+}
+
+func addMushroom() {
+	if mushrooms.Len() < mushroomSize {
+		mushrooms.PushBack(createMushroom())
+	}
+}
+
+func updateMushrooms(screen *ebiten.Image) {
+	addMushroom()
+
+	for e := mushrooms.Front(); e != nil; e = e.Next() {
+		m := mushroom(e.Value.(mushroom))
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(float64(m.xPos), float64(m.yPos))
+		screen.DrawImage(m.image, op)
+	}
+
+	for e := mushrooms.Front(); e != nil; e = e.Next() {
+		m := mushroom(e.Value.(mushroom))
+
+		if (playerOne.xPos <= m.xPos + catchSize && playerOne.xPos >= m.xPos - catchSize) &&
+			(playerOne.yPos <= m.yPos + catchSize && playerOne.yPos >= m.yPos - catchSize) {
+				mushrooms.Remove(e)
+				points += 20
+			}
+	}
+
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("Score: %d", points))
 }
 
 // Move the player depending on which key is pressed
 func movePlayer() {
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		playerOne.yPos -= playerOne.speed
+		if val := playerOne.yPos - playerOne.speed; val >= 0 {
+			playerOne.yPos -= playerOne.speed
+		}
 	}
+
 	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		playerOne.yPos += playerOne.speed
+		if val := playerOne.yPos + playerOne.speed; val <= screenHeight - characterCentering {
+			playerOne.yPos += playerOne.speed
+		}
 	}
+
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		playerOne.xPos -= playerOne.speed
+		if val := playerOne.xPos + playerOne.speed; val >= 0 {
+			playerOne.xPos -= playerOne.speed
+		}
 	}
+
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		playerOne.xPos += playerOne.speed
+		if val := playerOne.xPos + playerOne.speed; val <= screenWidth - characterCentering {
+			playerOne.xPos += playerOne.speed
+		}
 	}
 }
 
@@ -64,6 +133,7 @@ func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
+
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(0, 0)
 	screen.DrawImage(background, op)
@@ -71,6 +141,8 @@ func update(screen *ebiten.Image) error {
 	playerOp := &ebiten.DrawImageOptions{}
 	playerOp.GeoM.Translate(playerOne.xPos, playerOne.yPos)
 	screen.DrawImage(playerOne.image, playerOp)
+
+	updateMushrooms(screen)
 
 	return nil
 }
