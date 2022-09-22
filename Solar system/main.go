e60f8d1cd8ff24322725805af8dc251ca0d241e0
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-09-22 13:08:28 +0200

    move star, now planets have a rotation in the same plane also for stars

--- Solar system/main.go
+++ Solar system/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"log"
 	"math"
+	"image"
+	_ "image/png"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 const (
-	screenWidth, screenHeight = 900, 700
+	screenWidth, screenHeight = 2000, 1400
+
+
+	frameOX     = 0
+	frameOY     = 0
+	frameWidth  = 100
+	frameHeight = 100
+	frameCount  = 50
 )
 
 var (
@@ -18,25 +27,27 @@ var (
 )
 
 var (
-
-
 	imageTerra 	*ebiten.Image
-	imageLava  	*ebiten.Image
-	imageIce	*ebiten.Image
+	imageSolar 	*ebiten.Image
+	imageGiove 	*ebiten.Image
+	imageMars 	*ebiten.Image
 
-	terra planet
-	baren planet
-	lava planet
-	ice planet
+	terra 		planet
+	solar 		star
+	giove 		planet
+	mars 		planet
 )
 
 type planet struct {
 	image *ebiten.Image
 	radius float64
-	rotation float64
-	speed float64
-	scale float64
-	flag int
+	orbit, speedorbit float64
+	rotation, speedrotation float64
+}
+
+type star struct {
+	image *ebiten.Image
+	rotation, speedrotation float64
 }
 
 func sizeFloat64(img *ebiten.Image) (x float64, y float64) {
@@ -51,44 +62,52 @@ func init() {
 		log.Fatal(err)
 	}
 
-	imageTerra, _, err = ebitenutil.NewImageFromFile("assets/Terran.png", ebiten.FilterDefault)
+	imageTerra, _, err = ebitenutil.NewImageFromFile("assets/p1.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	imageLava, _, err = ebitenutil.NewImageFromFile("assets/Lava.png", ebiten.FilterDefault)
+	imageSolar, _, err = ebitenutil.NewImageFromFile("assets/Solar.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	imageIce, _, err = ebitenutil.NewImageFromFile("assets/Ice.png", ebiten.FilterDefault)
+	imageGiove, _, err = ebitenutil.NewImageFromFile("assets/p2.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	imageMars, _, err = ebitenutil.NewImageFromFile("assets/p3.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	terra = planet{imageTerra, 100, 0, 10, 0.5, 1}
-	lava = planet{imageLava, 250, 0, 6, 0.5, 1}
-	ice = planet{imageIce, 320, 0, 4, 0.5, 1}
+	terra = planet{imageTerra, 200, 0, 10, 0, 150}
+	giove = planet{imageGiove, 400, 0, 6, 0, 120}
+	mars = planet{imageMars, 600, 0, 14, 0, 140}
 
+	solar = star{imageSolar, 0, 150}
 }
 
 func movePlanet(globe *planet, screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(globe.scale, globe.scale)
-	op.GeoM.Translate(screenWidth/2 - globe.radius*math.Cos(globe.rotation), screenHeight/2 - globe.radius*math.Sin(globe.rotation))
-	screen.DrawImage(globe.image, op)
+	op.GeoM.Translate(screenWidth/2 - globe.radius*math.Cos(globe.orbit) - globe.radius/6, screenHeight/2 - globe.radius*math.Sin(globe.orbit) - globe.radius/6)
+	i := int(globe.rotation) % frameCount
+	sx, sy := i*frameWidth, frameOY
+	screen.DrawImage(globe.image.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), op)
 
-	if globe.scale <= 0 { 
-		globe.flag = 0
-	} else if globe.scale >= 1 {
-		globe.flag = 1
-	}
+	globe.rotation += globe.speedrotation/1000
+	globe.orbit += globe.speedorbit/1000
+}
 
-	if globe.flag == 0 {
-		globe.scale += globe.speed/5000
-	} else {
-		globe.scale -= globe.speed/5000
-	}
+func moveStar(star *star, screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(screenWidth/2 - frameWidth, screenHeight/2 - frameHeight)
+	i := int(star.rotation) % frameCount
+	sx, sy := i*frameWidth*2, frameOY
+	screen.DrawImage(star.image.SubImage(image.Rect(sx, sy, sx+200, sy+200)).(*ebiten.Image), op)
 
-	globe.rotation += globe.speed/1000
+	star.rotation += star.speedrotation/1000
 }
 
 func update(screen *ebiten.Image) error {
@@ -99,17 +118,19 @@ func update(screen *ebiten.Image) error {
 
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(0, 0)
+	op.GeoM.Scale(2,2)
 	screen.DrawImage(background, op)
 
 	movePlanet(&terra, screen)
-	movePlanet(&lava, screen)
-	movePlanet(&ice, screen)
+	movePlanet(&giove, screen)
+	movePlanet(&mars, screen)
+	moveStar(&solar, screen)
 
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "Solar system"); err != nil {
+	if err := ebiten.Run(update, screenWidth, screenHeight, 0.5, "Solar system"); err != nil {
 		log.Fatal(err)
 	}
 }
